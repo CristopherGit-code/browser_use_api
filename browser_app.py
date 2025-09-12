b6ecb44140f3e93d8df89952255048e49e33d603
Cristopher Hernandez <cristopher.hernandez@oracle.com>
2025-09-11 19:15:54 -0600

    Add credential submit

--- browser_app.py
+++ browser_app.py
@@ -5,8 +5,14 @@ from util.browser.browser_tools import BrowserAgentTools
 tool_watcher = BrowserAgentTools()
 
 async def use_browser(query,message_history):
-    bwsr_agent = BrowserUseAgent()
-    prompt = query + " Solve the previous query. If the query is not clear, ask the user for clarification"
+    user = "example"
+    password = "pass"
+    bwsr_agent = BrowserUseAgent(user,password)
+    prompt = f""" You are a browser agent in charge of solving the user request: {query}. 
+    There is this context availabe from previous user-agent interaction and conversations: {message_history}. 
+    Using the context, address the user query, use the browser tools to look for information and gather data.
+    You can also request the user for help in case there is a not clear step, or if there is extra details / information needed to complete the tastk.
+    """
     response = await bwsr_agent.use_browser(prompt)
     final_result = response.final_result()
     url_lists = response.urls()
@@ -17,26 +23,32 @@ async def use_browser(query,message_history):
 with gr.Blocks() as browser_app:
     gr.Markdown("""<h1 align="center"> Browser app </h1>""")
 
-    gr.ChatInterface(
-        fn=use_browser, 
-        chatbot=gr.Chatbot(height=500, placeholder='Browser chat' ,type='messages',render_markdown=True),
-        type="messages",
-    )
+    with gr.Tab("Browser agent"):
+        gr.ChatInterface(
+            fn=use_browser, 
+            chatbot=gr.Chatbot(height=500, placeholder='Browser chat' ,type='messages',render_markdown=True),
+            type="messages",
+        )
 
-    asking_help = gr.Textbox(interactive=False,label="AI is requesting help with this:")
-    user_response = gr.Textbox(placeholder="The response to the agent question is...",label="Type your response:")
+        asking_help = gr.Textbox(interactive=False,label="AI is requesting help with this:")
+        user_response = gr.Textbox(placeholder="The response to the agent question is...",label="Type your response:")
 
-    gr.Timer(value=0.5).tick(
-        fn=tool_watcher.update_question,
-        inputs=[],
-        outputs=[asking_help]
-    )
+        gr.Timer(value=0.5).tick(
+            fn=tool_watcher.update_question,
+            inputs=[],
+            outputs=[asking_help]
+        )
 
-    user_response.submit(
-        tool_watcher.update_human_response,
-        inputs=[user_response],
-        outputs=[user_response,asking_help]
-    )
+        user_response.submit(
+            tool_watcher.update_human_response,
+            inputs=[user_response],
+            outputs=[user_response,asking_help]
+        )
+
+    with gr.Tab("Secret keys"):
+        user = gr.Textbox(label="Site user")
+        password = gr.Textbox(label="Site password")
+        system_instructions = gr.Textbox(label="System instructions")
 
 if __name__=='__main__':
     browser_app.launch(show_api=False)
--- util/browser/browser_agent.py
+++ util/browser/browser_agent.py
@@ -8,23 +8,27 @@ from util.config.config import Settings
 from util.browser.browser_tools import tools
 
 class BrowserUseAgent:
-    def __init__(self):
+    def __init__(self,user,password):
         self._settings = Settings(r"C:\Users\Cristopher Hdz\Desktop\Test\browser_api\util\config\config.yaml")
-        self.sensitive_data = {'Clave unica':'example user','Contrasegna institucional':'dict_pass'}
         self._browser = Browser(
             headless=False,
-            window_size={'width':500,'height':700},
-            allowed_domains=['*google.com','didactic.uaslp.mx'],
+            window_size={'width':600,'height':300},
+            # allowed_domains=['*google.com'],
+            keep_alive=True
         )
         self._browser_profile = BrowserProfile(
-            allowed_domains=['*google.com','didactic.uaslp.mx'],
+            allowed_domains=['*google.com'],
             enable_default_extensions=False,
         )
         self._oci_langchain_client = LLM_Open_Client().build_llm_client()
         self._browser_llm = ChatLangchain(
             chat=self._oci_langchain_client
         )
+        # To inject the data, not passing to LLM
+        self._sensitive_data = {"gmail_user":user,"gmail_password":password}
         self.tools = tools
+        #TODO: Set to false to prevent AI from seeing screenshots of the password
+        self._use_vision = True
 
     async def use_browser(self,task:str):
         agent_task = task
@@ -32,7 +36,10 @@ class BrowserUseAgent:
         agent = Agent(
             task=agent_task,
             llm=self._browser_llm,
-            tools=self.tools
+            tools=self.tools,
+            browser=self._browser,
+            sensitive_data=self._sensitive_data,
+            use_vision=self._use_vision
         )
         
         history = await agent.run()
